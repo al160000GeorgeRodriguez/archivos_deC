adb7d1ee3e5e3a441c45fa2465654a8cb83258db
geoxneo <al160000@alumnos.uacj.mx>
2017-03-02 18:06:07 -0700

    Signed-off-by: geoxneo <al160000@alumnos.uacj.mx>

--- Ejercicios en C/mod.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using std::cout;
-using std::endl;
-using std::cin;
-int main (){
-	int b;
-	b=14%101;
-cout<<b;
-}
--- Ejercicios en C/welcome.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <iostream>
-int main()
- {
-std::cout<< "Welcome to C++!\n";
-std::cout<<"to c++\n";
-return 0;
-}
